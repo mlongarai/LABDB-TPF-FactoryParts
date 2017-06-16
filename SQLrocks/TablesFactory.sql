48b5ef21d715f55941f1f8dcb1340dad18db158a
matheusvp0 <mventurini@outlook.com>
2017-06-16 18:29:36 -0300

    Update TablesFactory.sql
    Atualizados valores unicos, acrescentado ponto e virgula ao final de cada create.

--- SQLrocks/TablesFactory.sql
+++ SQLrocks/TablesFactory.sql
@@ -3,24 +3,24 @@
 
 CREATE TABLE FILIAL(
 ID number PRIMARY KEY,
-cnpj_filial CHAR(11),
+cnpj_filial CHAR(11) UNIQUE,
 cidade VARCHAR(50)
-)
+);
 
 CREATE TABLE VENDEDORES(
 ID number PRIMARY KEY,
 idFilial INT REFERENCES FILIAL(ID),
 nomeVendedor VARCHAR(50),
-cpf CHAR(11),
+cpf CHAR(11) UNIQUE,
 endereco VARCHAR(50)
-)
+);
 
 CREATE TABLE CLIENTES(
 ID number PRIMARY KEY,
 nomeCliente VARCHAR(50),
 cpf CHAR(11),
 endereco VARCHAR(50)
-)
+);
 
 CREATE TABLE PEDIDOS(
 ID number PRIMARY KEY,
@@ -29,14 +29,14 @@ idCliente INT REFERENCES CLIENTES(ID),
 valorPedido int,
 dataPedido date,
 dataEntrega date
-)
+);
 
 CREATE TABLE VENDAS(
 ID number PRIMARY KEY,
 idPedido INT REFERENCES PEDIDOS(ID),
 dataVenda date,
 valorVenda int
-)
+);
 
 CREATE TABLE PRODUTOS(
 ID number PRIMARY KEY,
@@ -44,14 +44,14 @@ nomeProduto VARCHAR(50),
 custoProduto int,
 precoProduto int,
 qtdProduto INT CHECK(qtdProduto > 0)
-)
+);
 
 CREATE TABLE PECAS(
 ID number PRIMARY KEY,
 nomePeca VARCHAR(50),
 custoPeca int,
 qtdPeca INT CHECK (qtdPeca >0)
-)
+);
 
 CREATE TABLE MONTAGEM(
 ID number PRIMARY KEY,
@@ -60,5 +60,5 @@ idProduto INT REFERENCES PRODUTOS(ID),
 idPeca INT REFERENCES PECAS(ID),
 valorProduto int,
 quantidadeProduto INT CHECK (quantidadeProduto > 0)
-)
+);
 
